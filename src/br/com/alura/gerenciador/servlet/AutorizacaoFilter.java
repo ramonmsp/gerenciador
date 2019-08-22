8f925edfbd0581ba1740c135d15a7e8386b48278
Ramon Maciel <msp.ramon@gmail.com>
2019-08-22 15:47:07 -0300

    Finalização. inclusos filtros, xml, json no webservice

--- src/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
+++ src/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -12,12 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Servlet Filter implementation class AutorizacaoFilter
- */
 @WebFilter("/entrada")
 public class AutorizacaoFilter implements Filter {
 
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
+	
+	@Override
+	public void destroy() {}
+	
+	
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 		
 		HttpServletRequest request = (HttpServletRequest) servletRequest;
